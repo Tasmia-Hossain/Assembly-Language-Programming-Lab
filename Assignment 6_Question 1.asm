3d7ee59ab98eadd6d315f224e3cb133bef792d3e
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-01-20 11:48:03 +0600

    Update Assignment 6_Question 1.asm
    Write a program that will prompt the user to enter a hex digit character (0- F), display it on the next line in decimal and ask the user if he or she wants to do it again. If the user types “Y”, the program repeats. If the user types “N” the program terminates. If the user enters an illegal character prompt the user to try again.

--- Assignment 6_Question 1.asm
+++ Assignment 6_Question 1.asm
@@ -1,3 +1,7 @@
+; Write a program that will prompt the user to enter a hex digit character (0- F), display it on the next line in decimal and ask the user if he or she wants to
+; do it again. If the user types “Y”, the program repeats. If the user types “N” the program terminates. If the user enters an illegal character prompt the
+; user to try again.
+
 .MODEL SMALL
 .STACK 100H
 .DATA
