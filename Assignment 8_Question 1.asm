6b254288683bd1b9b56b8b2a05ec7f630988c214
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-01-20 11:56:35 +0600

    Update Assignment 8_Question 1.asm
    Write a program that lets the user type in an algebraic expression, ending with a carriage return, that contains round, square, and curly brackets. As the expression is being typed in, the program evaluates each character. If at any point the expression is incorrectly bracketed (too many right brackets or a mismatch between left and right brackets), the program tells the user to start over. After the carriage return is typed, if the expression is correct, the program displays "expression is correct." If not, the program displays "too many left brackets". In
    both cases, the program asks the user if he or she wants to continue. If the user types 'Y', the program runs again. Your program does not need to store the input string, only check it for
    correctness.

--- Assignment 8_Question 1.asm
+++ Assignment 8_Question 1.asm
@@ -1,3 +1,10 @@
+; Write a program that lets the user type in an algebraic expression, ending with a carriage return, that contains round, square, and curly brackets.
+; As the expression is being typed in, the program evaluates each character. 
+; If at any point the expression is incorrectly bracketed (too many right brackets or a mismatch between left and right brackets), the program tells the user to start over. 
+; After the carriage return is typed, if the expression is correct, the program displays "expression is correct." 
+; If not, the program displays "too many left brackets". In both cases, the program asks the user if he or she wants to continue. 
+; If the user types 'Y', the program runs again. Your program does not need to store the input string, only check it for correctness.
+
 .MODEL SMALL
 .STACK 100H
 .DATA  
