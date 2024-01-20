cba136e0e99c98d3b877659b18a4cf0facd68a09
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-01-20 11:58:21 +0600

    Update Assignment 8_Question 3.asm
    Write a program to find the greatest common divisor (GCD) of two integers M and N, according to the following algorithm:
    • Divide M by N, getting quotient Q and remainder R.
    • If R = 0 then stop. N is the GCD of M and N.
    • If R <> 0 replace M by N, N by R, and repeat step 1.

--- Assignment 8_Question 3.asm
+++ Assignment 8_Question 3.asm
@@ -1,3 +1,8 @@
+; Write a program to find the greatest common divisor (GCD) of two integers M and N, according to the following algorithm:
+; • Divide M by N, getting quotient Q and remainder R.
+; • If R = 0 then stop. N is the GCD of M and N.
+; • If R <> 0 replace M by N, N by R, and repeat step 1.
+
 .MODEL SMALL
 .STACK 100H
 .DATA
